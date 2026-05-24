630a6a9e7c28cb710ff41feeb4df5370c051f373
nn26328na-ui <nn26328.na@gmail.com>
2026-05-24 13:22:22 +0800

    Update mock_api.py

--- mock_api.py
+++ mock_api.py
@@ -99,5 +99,7 @@ def health():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    import os
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
 
